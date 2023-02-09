2a66c996468a6dd94105005536d029bbf884c418
Dashrath-Sharma <dashrathsharma7838@gmail.com>
2023-02-09 18:45:07 +0530

    Basic delete and read functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DATA from './data';
 
 function App() {
+  const [answer, setAnswer] = useState('')
+  const [words, setWords] = useState(Array(6).fill(null))
+  const [currentGuess, setCurrentGuess] = useState('')
+
+  useEffect(() => {
+    setAnswer(DATA[Math.floor(Math.random() * DATA.length)])
+  }, [])
+
+  const handleKeys = (event) => {
+    const pattern = /^[a-zA-Z]$/g
+    const key = event.key
+    if(currentGuess.length == 5) return
+    if (key.match(pattern))
+      setCurrentGuess(guess => guess + key)
+    else if (key.match("Backspace"))
+      setCurrentGuess(guess => guess.slice(0, -1))
+    else if (key.match("Enter"))
+      console.log("Enter was pressed")
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeys)
+    return () => window.addEventListener('keydown', handleKeys)
+  }, [])
+
   return (
     <div className="App">
       Wordle game
+      <div>{answer}</div>
+      <div>{currentGuess}</div>
     </div>
   );
 }
